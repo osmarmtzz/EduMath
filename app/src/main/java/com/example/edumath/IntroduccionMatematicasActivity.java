8d5d79cd743242ebc75cb5f499e6d25ea019be63
Osmar <osmar_935@hotmail.com>
2024-11-04 07:27:34 -0600

    Cambios en los layouts se agrego la funcionalidad de poder regresar entre menu y las opciones y diseño en instrucciones geometria y matematicas

--- app/src/main/java/com/example/edumath/IntroduccionMatematicasActivity.java
+++ app/src/main/java/com/example/edumath/IntroduccionMatematicasActivity.java
@@ -1,4 +1,12 @@
 package com.example.edumath;
 
-public class IntroduccionMatematicasActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroduccionMatematicasActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduccion_matematicas);
+    }
 }
