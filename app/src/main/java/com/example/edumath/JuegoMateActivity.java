7088c0e9c24caff2ed942e9ed81cf844b0125869
Osmar <osmar_935@hotmail.com>
2024-11-23 12:17:28 -0600

    Cambios en juego mate diseño y funcioanlidad

--- app/src/main/java/com/example/edumath/JuegoMateActivity.java
+++ app/src/main/java/com/example/edumath/JuegoMateActivity.java
@@ -1,6 +1,5 @@
 package com.example.edumath;
 
-import android.animation.ObjectAnimator;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,10 +8,11 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.CountDownTimer;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -21,130 +21,168 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 public class JuegoMateActivity extends AppCompatActivity implements SensorEventListener {
-    private TextView problemTexto, puntuacionTexto, tiempoTexto, nivelTexto, comboTexto, vidasTexto;
-    private Button[] botonesRespuesta;
-    private ProgressBar barraProgreso;
 
-    private int puntuacion = 0;
-    private int respuestaCorrecta;
-    private int nivelActual = 1;
-    private int comboActual = 0;
-    private int vidasRestantes = 3;
-    private int ejerciciosResueltos = 0;
+    private TextView problemaTexto, puntuacionTexto, tiempoTexto, vidasTexto, comboTexto;
+    private ProgressBar barraTiempo;
+    private Button[] botonesRespuesta;
+    private GridLayout gridRespuestas;
+
+    private int puntuacion = 0, ejerciciosResueltos = 0, vidasRestantes = 3, comboActual = 0;
     private boolean segundaDinamica = false;
+    private int respuestaCorrecta;
+    private CountDownTimer temporizador;
 
     private Random generadorAleatorio;
-    private CountDownTimer timer;
-
     private SensorManager sensorManager;
     private Sensor acelerometro;
-
-    private static final long TIEMPO_POR_PREGUNTA = 15000; // 15 segundos
+    private static final float UMBRAL_ACELEROMETRO = 3.0f;
+    private long ultimoMovimiento = 0;
+    private static final long DELAY_ENTRE_MOVIMIENTOS = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.juego_mate);
 
-        inicializarComponentes();
-        iniciarJuego();
-    }
-
-    private void inicializarComponentes() {
-        problemTexto = findViewById(R.id.problema_texto);
+        problemaTexto = findViewById(R.id.problema_texto);
         puntuacionTexto = findViewById(R.id.puntuacion_texto);
         tiempoTexto = findViewById(R.id.tiempo_texto);
-        nivelTexto = findViewById(R.id.nivel_texto);
-        comboTexto = findViewById(R.id.combo_texto);
         vidasTexto = findViewById(R.id.vidas_texto);
-        barraProgreso = findViewById(R.id.barra_tiempo);
+        comboTexto = findViewById(R.id.combo_texto);
+        barraTiempo = findViewById(R.id.barra_tiempo);
+        gridRespuestas = findViewById(R.id.grid_respuestas);
 
-        botonesRespuesta = new Button[4];
-        botonesRespuesta[0] = findViewById(R.id.boton_respuesta1);
-        botonesRespuesta[1] = findViewById(R.id.boton_respuesta2);
-        botonesRespuesta[2] = findViewById(R.id.boton_respuesta3);
-        botonesRespuesta[3] = findViewById(R.id.boton_respuesta4);
+        botonesRespuesta = new Button[]{
+                findViewById(R.id.boton_respuesta1),
+                findViewById(R.id.boton_respuesta2),
+                findViewById(R.id.boton_respuesta3),
+                findViewById(R.id.boton_respuesta4)
+        };
 
         generadorAleatorio = new Random();
+        configurarBotones();
 
-        for (Button boton : botonesRespuesta) {
-            boton.setOnClickListener(v -> verificarRespuesta(Integer.parseInt(boton.getText().toString())));
-        }
-
-        // Configuración del sensor de acelerómetro
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (sensorManager != null) {
-            acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        }
-    }
+        acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-    private void iniciarJuego() {
-        actualizarInterfaz();
         generarNuevoProblema();
         iniciarTemporizador();
     }
 
+    private void configurarBotones() {
+        for (int i = 0; i < botonesRespuesta.length; i++) {
+            int finalI = i;
+            botonesRespuesta[i].setOnClickListener(v -> verificarRespuesta(finalI));
+        }
+    }
+
     private void generarNuevoProblema() {
-        if (segundaDinamica) {
-            generarProblemaComparativo();
-        } else {
+        if (ejerciciosResueltos < 5) {
+            segundaDinamica = false;
+            mostrarBotones(4);
             generarProblemaMatematico();
+        } else {
+            segundaDinamica = true;
+            mostrarBotones(2);
+            generarProblemaComparativo();
         }
     }
 
     private void generarProblemaMatematico() {
-        int[] operaciones = {1, 2, 3, 4}; // 1:suma, 2:resta, 3:multiplicación, 4:división
-        int tipoOperacion = operaciones[generadorAleatorio.nextInt(operaciones.length)];
+        int num1 = generadorAleatorio.nextInt(20) + 1;
+        int num2 = generadorAleatorio.nextInt(15) + 1;
+        int operacion = generadorAleatorio.nextInt(4);
 
-        int rangoNumeros = 10 + (nivelActual * 5);
-        int num1 = generadorAleatorio.nextInt(rangoNumeros) + 1;
-        int num2 = generadorAleatorio.nextInt(rangoNumeros) + 1;
-
-        switch (tipoOperacion) {
-            case 1:
-                problemTexto.setText(num1 + " + " + num2 + " = ?");
-                respuestaCorrecta = num1 + num2;
+        int resultado;
+        switch (operacion) {
+            case 0: // Suma
+                problemaTexto.setText(num1 + " + " + num2);
+                resultado = num1 + num2;
                 break;
-            case 2:
-                if (num1 < num2) {
+            case 1: // Resta
+                if (num2 > num1) {
                     int temp = num1;
                     num1 = num2;
                     num2 = temp;
                 }
-                problemTexto.setText(num1 + " - " + num2 + " = ?");
-                respuestaCorrecta = num1 - num2;
+                problemaTexto.setText(num1 + " - " + num2);
+                resultado = num1 - num2;
                 break;
-            case 3:
-                problemTexto.setText(num1 + " × " + num2 + " = ?");
-                respuestaCorrecta = num1 * num2;
+            case 2: // Multiplicación
+                num1 = generadorAleatorio.nextInt(12) + 1;
+                num2 = generadorAleatorio.nextInt(10) + 1;
+                problemaTexto.setText(num1 + " × " + num2);
+                resultado = num1 * num2;
                 break;
-            case 4:
-                num1 = num2 * (generadorAleatorio.nextInt(rangoNumeros / 2) + 1);
-                problemTexto.setText(num1 + " ÷ " + num2 + " = ?");
-                respuestaCorrecta = num1 / num2;
+            default: // División
+                num2 = generadorAleatorio.nextInt(10) + 1;
+                resultado = generadorAleatorio.nextInt(10) + 1;
+                num1 = num2 * resultado;
+                problemaTexto.setText(num1 + " ÷ " + num2);
                 break;
         }
-        generarOpciones();
+
+        int posicionCorrecta = generadorAleatorio.nextInt(4);
+        for (int i = 0; i < 4; i++) {
+            if (i == posicionCorrecta) {
+                botonesRespuesta[i].setText(String.valueOf(resultado));
+            } else {
+                int incorrecto = resultado + (generadorAleatorio.nextInt(10) - 5);
+                while (incorrecto == resultado || incorrecto < 0) {
+                    incorrecto = resultado + (generadorAleatorio.nextInt(10) - 5);
+                }
+                botonesRespuesta[i].setText(String.valueOf(incorrecto));
+            }
+        }
+        respuestaCorrecta = posicionCorrecta;
     }
 
     private void generarProblemaComparativo() {
-        int num1 = generadorAleatorio.nextInt(100);
-        int num2 = generadorAleatorio.nextInt(100);
+        int num1 = generadorAleatorio.nextInt(20) + 1;
+        int num2 = generadorAleatorio.nextInt(15) + 1;
+        int operacion = generadorAleatorio.nextInt(4);
 
-        respuestaCorrecta = num1 > num2 ? 1 : -1;
+        int resultadoCorrecto;
+        switch (operacion) {
+            case 0:
+                problemaTexto.setText(num1 + " + " + num2);
+                resultadoCorrecto = num1 + num2;
+                break;
+            case 1:
+                if (num2 > num1) {
+                    int temp = num1;
+                    num1 = num2;
+                    num2 = temp;
+                }
+                problemaTexto.setText(num1 + " - " + num2);
+                resultadoCorrecto = num1 - num2;
+                break;
+            case 2:
+                num1 = generadorAleatorio.nextInt(12) + 1;
+                num2 = generadorAleatorio.nextInt(10) + 1;
+                problemaTexto.setText(num1 + " × " + num2);
+                resultadoCorrecto = num1 * num2;
+                break;
+            default:
+                num2 = generadorAleatorio.nextInt(10) + 1;
+                resultadoCorrecto = generadorAleatorio.nextInt(10) + 1;
+                num1 = num2 * resultadoCorrecto;
+                problemaTexto.setText(num1 + " ÷ " + num2);
+                break;
+        }
 
-        problemTexto.setText(num1 + " vs " + num2 + " (Izq: menor, Der: mayor)");
+        // Ahora siempre ponemos la respuesta correcta a la derecha
+        respuestaCorrecta = 1;
+        int incorrecto = resultadoCorrecto + (generadorAleatorio.nextInt(5) + 1);
+
+        // La respuesta correcta siempre va a la derecha
+        botonesRespuesta[0].setText(String.valueOf(incorrecto));
+        botonesRespuesta[1].setText(String.valueOf(resultadoCorrecto));
     }
 
-    private void generarOpciones() {
-        int[] opciones = new int[4];
-        opciones[generadorAleatorio.nextInt(4)] = respuestaCorrecta;
-
-        for (int i = 0; i < opciones.length; i++) {
-            if (opciones[i] == 0) {
-                opciones[i] = respuestaCorrecta + (generadorAleatorio.nextInt(5) - 2);
-            }
-            botonesRespuesta[i].setText(String.valueOf(opciones[i]));
+    private void mostrarBotones(int cantidad) {
+        for (int i = 0; i < botonesRespuesta.length; i++) {
+            botonesRespuesta[i].setVisibility(i < cantidad ? View.VISIBLE : View.GONE);
         }
     }
 
@@ -152,66 +190,73 @@ public class JuegoMateActivity extends AppCompatActivity implements SensorEventL
         cancelarTemporizador();
 
         if (respuestaSeleccionada == respuestaCorrecta) {
-            puntuacion += 10 + comboActual * 2;
-            comboActual++;
-            ejerciciosResueltos++;
-
-            if (ejerciciosResueltos % 5 == 0) {
-                segundaDinamica = !segundaDinamica;
-            }
+            actualizarPuntuacion();
         } else {
-            vidasRestantes--;
-            comboActual = 0;
-
-            if (vidasRestantes <= 0) {
-                mostrarDialogoFinJuego();
-                return;
-            }
+            decrementarVida();
         }
 
-        actualizarInterfaz();
+        if (vidasRestantes <= 0) {
+            mostrarDialogoFinJuego();
+            return;
+        }
+
         generarNuevoProblema();
         iniciarTemporizador();
     }
 
-    private void iniciarTemporizador() {
-        barraProgreso.setMax(100);
-        barraProgreso.setProgress(100);
+    private void actualizarPuntuacion() {
+        puntuacion += 10 + comboActual * 2;
+        comboActual++;
+        ejerciciosResueltos++;
+        actualizarInterfaz();
+    }
 
-        timer = new CountDownTimer(TIEMPO_POR_PREGUNTA, 100) {
+    private void decrementarVida() {
+        vidasRestantes--;
+        comboActual = 0;
+        actualizarInterfaz();
+    }
+
+    private void actualizarInterfaz() {
+        puntuacionTexto.setText("Puntuación: " + puntuacion);
+        comboTexto.setText("Combo: x" + comboActual);
+        vidasTexto.setText("Vidas: " + vidasRestantes);
+    }
+
+    private void iniciarTemporizador() {
+        temporizador = new CountDownTimer(15000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                int progreso = (int) (millisUntilFinished * 100 / TIEMPO_POR_PREGUNTA);
-                barraProgreso.setProgress(progreso);
-                tiempoTexto.setText(String.format("%.1f", millisUntilFinished / 1000.0) + "s");
+                tiempoTexto.setText(millisUntilFinished / 1000 + "s");
+                barraTiempo.setProgress((int) (millisUntilFinished / 150));
             }
 
             @Override
             public void onFinish() {
-                vidasRestantes--;
-                if (vidasRestantes <= 0) {
-                    mostrarDialogoFinJuego();
-                } else {
+                decrementarVida();
+                if (vidasRestantes > 0) {
                     generarNuevoProblema();
                     iniciarTemporizador();
+                } else {
+                    mostrarDialogoFinJuego();
                 }
             }
-        }.start();
+        };
+        temporizador.start();
     }
 
     private void cancelarTemporizador() {
-        if (timer != null) {
-            timer.cancel();
+        if (temporizador != null) {
+            temporizador.cancel();
         }
     }
 
     private void mostrarDialogoFinJuego() {
-        cancelarTemporizador();
         new AlertDialog.Builder(this)
-                .setTitle("Fin del Juego")
-                .setMessage("Puntuación final: " + puntuacion + "\nNivel alcanzado: " + nivelActual)
-                .setPositiveButton("Reiniciar", (dialog, which) -> reiniciarJuego())
-                .setNegativeButton("Salir", (dialog, which) -> finish())
+                .setTitle("¡Juego terminado!")
+                .setMessage("Puntuación final: " + puntuacion)
+                .setPositiveButton("Jugar de nuevo", (dialog, which) -> reiniciarJuego())
+                .setNegativeButton("Salir al menú", (dialog, which) -> finish())
                 .setCancelable(false)
                 .show();
     }
@@ -219,34 +264,54 @@ public class JuegoMateActivity extends AppCompatActivity implements SensorEventL
     private void reiniciarJuego() {
         puntuacion = 0;
         vidasRestantes = 3;
-        nivelActual = 1;
         comboActual = 0;
         ejerciciosResueltos = 0;
         segundaDinamica = false;
-        iniciarJuego();
+        actualizarInterfaz();
+        generarNuevoProblema();
+        iniciarTemporizador();
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (segundaDinamica && event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long tiempoActual = System.currentTimeMillis();
+
+            if (tiempoActual - ultimoMovimiento < DELAY_ENTRE_MOVIMIENTOS) {
+                return;
+            }
+
             float x = event.values[0];
-            if (x > 5) { // Derecha
-                verificarRespuesta(1);
-            } else if (x < -5) { // Izquierda
-                verificarRespuesta(-1);
+
+            if (Math.abs(x) > UMBRAL_ACELEROMETRO) {
+                ultimoMovimiento = tiempoActual;
+
+                // x positivo = inclinación izquierda, x negativo = inclinación derecha
+                if (x > UMBRAL_ACELEROMETRO) { // Inclinación izquierda
+                    botonesRespuesta[0].setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
+                    new Handler().postDelayed(() -> {
+                        botonesRespuesta[0].setBackgroundColor(getResources().getColor(android.R.color.background_light));
+                        verificarRespuesta(0);
+                    }, 200);
+                } else if (x < -UMBRAL_ACELEROMETRO) { // Inclinación derecha
+                    botonesRespuesta[1].setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
+                    new Handler().postDelayed(() -> {
+                        botonesRespuesta[1].setBackgroundColor(getResources().getColor(android.R.color.background_light));
+                        verificarRespuesta(1);
+                    }, 200);
+                }
             }
         }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (segundaDinamica && acelerometro != null) {
-            sensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL);
+        if (acelerometro != null) {
+            sensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_GAME);
         }
     }
 
@@ -255,12 +320,5 @@ public class JuegoMateActivity extends AppCompatActivity implements SensorEventL
         super.onPause();
         sensorManager.unregisterListener(this);
         cancelarTemporizador();
-    }
-
-    private void actualizarInterfaz() {
-        puntuacionTexto.setText("Puntuación: " + puntuacion);
-        nivelTexto.setText("Nivel: " + nivelActual);
-        comboTexto.setText("Combo: x" + comboActual);
-        vidasTexto.setText("Vidas: " + vidasRestantes);
     }
 }
